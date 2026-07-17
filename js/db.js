1b16b155ba9c9aee443a87951f91ed659b68094c
solarisken <solarisken@gmail.com>
2026-07-17 09:52:19 +0800

    Add files via upload

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-const DB='beylab-rebuild-v1',VER=1,STORES=['parts','productsOwned','builds','tests','matches','settings'];
+const DB='beylab-rebuild-v1',VER=2,STORES=['parts','productsOwned','builds','tests','matches','settings','projects','notebook'];
 function open(){return new Promise((res,rej)=>{const r=indexedDB.open(DB,VER);r.onupgradeneeded=()=>{for(const s of STORES)if(!r.result.objectStoreNames.contains(s))r.result.createObjectStore(s,{keyPath:'id'})};r.onsuccess=()=>res(r.result);r.onerror=()=>rej(r.error)})}
 export async function all(s){const d=await open();return new Promise((res,rej)=>{const r=d.transaction(s).objectStore(s).getAll();r.onsuccess=()=>res(r.result);r.onerror=()=>rej(r.error)})}
 export async function put(s,v){const d=await open();return new Promise((res,rej)=>{const t=d.transaction(s,'readwrite');t.objectStore(s).put(v);t.oncomplete=()=>res();t.onerror=()=>rej(t.error)})}
