2cbc1c729a480cb6b3b94e7e2779f01d01faf259
solarisken <solarisken@gmail.com>
2026-07-18 14:49:55 +0800

    Delete router.js

--- router.js
+++
@@ -1,4 +0,0 @@
-const ROUTES=new Set(['collection','deck','test','match']);
-export function currentRoute(){const value=location.hash.replace(/^#\/?/,'').split('/')[0];return ROUTES.has(value)?value:'collection'}
-export function go(route){if(!ROUTES.has(route))route='collection';location.hash=`#/${route}`}
-export function startRouter(render){addEventListener('hashchange',()=>render(currentRoute()));if(!location.hash)go('collection');else render(currentRoute())}
